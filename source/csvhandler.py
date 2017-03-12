86c4f41e883ad1caf51ac4b69af618fbdd95fe37
csvk <chanda.souvik@gmail.com>
2017-03-12 12:48:52 +0530

    CSV Formatting

--- source/csvhandler.py
+++ source/csvhandler.py
@@ -10,7 +10,7 @@ import os
 import dates, utils
 import pandas as pd
 
-PATH = 'C:/Users/Souvik/OneDrive/Python/mcxdata/data/' # Laptop
+PATH = 'C:/Users/Souvik/OneDrive/Python/mcxdata/data new/' # Laptop
 FORMATTED = 'formatted/'
 
 def format_csv_futures(path, *columns):
@@ -28,18 +28,19 @@ def format_csv_futures(path, *columns):
         try:
             df = pd.read_csv(file)
             date = dates.ddmmyyyy_to_yyyy_mm_dd(file[-12:][:8]) # Extract date from filename
+            df['Expiry Date'] = df['Expiry Date'].apply(dates.ddMMMyyyy_to_yyyy_mm_dd)  # Update Expiry Date Format
             df = df.reindex_axis(cols, axis=1)
             df.to_csv('{}{}'.format(FORMATTED, file), sep=',', index=False)
-            print(date, ',File Cleaned', file)
+            print(date, ',File formatted', file)
         except:
-            print(date, ',Error in cleaning', file)
+            print(date, ',Error in formatting', file)
 
 
 def ren_csv_files(path):
 
     os.chdir(path)
 
-    csv_files = [f for f in os.listdir(os.curdir) if f.find('OP') < 0 and f.endswith('.csv')]
+    csv_files = [f for f in os.listdir(os.curdir) if f.endswith('.csv')]
 
     for file in csv_files:
         try:
